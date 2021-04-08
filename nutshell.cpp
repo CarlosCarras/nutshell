9e873bb181b9d609bbca54a341e2da7178ded55a
Carlos Carrasquillo <carlos.carrasquillo99@gmail.com>
2021-04-08 19:11:30 +0000

    fixed race condition prompt printout

--- nutshell.cpp
+++ nutshell.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <unistd.h>
 
-// #define BLUE   "\033[0;34m"
-// #define GREEN  "\033[0;32m"
-// #define CYAN   "\033[0;36m"
-// #define WHITE  "\033[0;37m"
-// #define PURPLE "\033[0;35m"
-// #define RESET  "\x1B[0m"
+#define BLUE   "\033[0;34m"
+#define GREEN  "\033[0;32m"
+#define CYAN   "\033[0;36m"
+#define WHITE  "\033[0;37m"
+#define PURPLE "\033[0;35m"
+#define RESET  "\x1B[0m"
 
 
 int main() {
@@ -17,13 +18,14 @@ int main() {
     system("clear");            // clears terminal window
 
     while(1) {
-        // getcwd(cwd, sizeof(cwd));
-        // printf(BLUE "nutshell");
-        // printf(WHITE ":");
-        // printf(GREEN "%s", cwd);
-        // printf(WHITE ">> " RESET);
+        getcwd(cwd, sizeof(cwd));
+        printf(BLUE "nutshell");
+        printf(WHITE ":");
+        printf(GREEN "%s", cwd);
+        printf(WHITE ">> " RESET);
 
         yyparse();
+        usleep(100);
     }
     return 0;
 }
